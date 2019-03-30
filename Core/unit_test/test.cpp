818628b231caa6e6175ce20819b9ac41a48aba62
ZhangJingtun <zhangjingtun@gmail.com>
2019-03-30 11:35:28 +0800

    在unit_test/下make,直接输出覆盖率（Chrome浏览器，命令行google-chrome 打开的那种）

--- Core/unit_test/test.cpp
+++ Core/unit_test/test.cpp
@@ -1,9 +1,8 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
-//#include "../include/Core.h"
 #include "../CoreImplement.h"
 
-TEST_CASE("standard test"){
+TEST_CASE("standard test for lab1-b"){
     CoreImplement coreTest;
     std::vector<std::string> input = {"END","OF","THE","WORLD"};
     std::vector<std::string> result;
@@ -12,4 +11,75 @@ TEST_CASE("standard test"){
     
     int len = coreTest.gen_chain_word(input,result,head,tail);
     REQUIRE(len == 2);
+    REQUIRE(result[0] == "the");
+    REQUIRE(result[1] == "end");
+}
+
+TEST_CASE("standard test for lab1 longest word"){
+    CoreImplement coreTest;
+    std::vector<std::string> input = {"Algebra", "Apple", "Zoo", "Elephant", "Under", "Fox", "Dog", "Moon", "Leaf", "Trick", "Pseudopseudohypoparathyroidism"};
+    std::vector<std::string> result;
+    char head = 0;
+    char tail = 0;
+
+    int len = coreTest.gen_chain_word(input,result,head,tail);
+    REQUIRE(len == 4);
+    REQUIRE(result[0] == "algebra");
+    REQUIRE(result[1] == "apple");
+    REQUIRE(result[2] == "elephant");
+    REQUIRE(result[3] == "trick");
+}
+
+TEST_CASE("standard test for lab1 longest char"){
+    CoreImplement coreTest;
+    std::vector<std::string> input = {"Algebra", "Apple", "Zoo", "Elephant", "Under", "Fox", "Dog", "Moon", "Leaf", "Trick", "Pseudopseudohypoparathyroidism"};
+    std::vector<std::string> result;
+    char head = 0;
+    char tail = 0;
+
+    int len = coreTest.gen_chain_char(input,result,head,tail);
+    REQUIRE(len == 2);
+    //REQUIRE(result[0] == "pseudopseudohypoparathyroidism");
+    //REQUIRE(result[1] == "moon");
+}
+
+TEST_CASE("standard test for lab1 longest word fixed head"){
+    CoreImplement coreTest;
+    std::vector<std::string> input = {"Algebra", "Apple", "Zoo", "Elephant", "Under", "Fox", "Dog", "Moon", "Leaf", "Trick", "Pseudopseudohypoparathyroidism"};
+    std::vector<std::string> result;
+    char head = 'e';
+    char tail = 0;
+
+    int len = coreTest.gen_chain_word(input,result,head,tail);
+    REQUIRE(len == 2);
+    REQUIRE(result[0] == "elephant");
+    REQUIRE(result[1] == "trick");
+}
+
+TEST_CASE("standard test for lab1 longest word fixed tail"){
+    CoreImplement coreTest;
+    std::vector<std::string> input = {"Algebra", "Apple", "Zoo", "Elephant", "Under", "Fox", "Dog", "Moon", "Leaf", "Trick", "Pseudopseudohypoparathyroidism"};
+    std::vector<std::string> result;
+    char head = 0;
+    char tail = 't';
+
+    int len = coreTest.gen_chain_word(input,result,head,tail);
+    REQUIRE(len == 3);
+    REQUIRE(result[0] == "algebra");
+    REQUIRE(result[1] == "apple");
+    REQUIRE(result[2] == "elephant");
+}
+
+TEST_CASE("standard test for lab1 longest word fixed head & tail"){
+    CoreImplement coreTest;
+    std::vector<std::string> input = {"Algebra", "Apple", "Zoo", "Elephant", "Under", "Fox", "Dog", "Moon", "Leaf", "Trick", "Pseudopseudohypoparathyroidism"};
+    std::vector<std::string> result;
+    char head = 'a';
+    char tail = 't';
+
+    int len = coreTest.gen_chain_word(input,result,head,tail);
+    REQUIRE(len == 3);
+    REQUIRE(result[0] == "algebra");
+    REQUIRE(result[1] == "apple");
+    REQUIRE(result[2] == "elephant");
 }
